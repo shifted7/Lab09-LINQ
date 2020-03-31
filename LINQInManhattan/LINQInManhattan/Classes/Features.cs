4a49057a108bdb2050b04463124f705342dda144
Andrew <shifted6@gmail.com>
2020-03-30 23:48:28 -0700

    Created files, started on feature tasks.

---
+++ LINQInManhattan/LINQInManhattan/Classes/Features.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQInManhattan.Classes
+{
+    class dataObject
+    {
+        public string Type { get; set; }
+        public List<Feature> Features { get; set; }
+
+    }
+
+    public class Feature
+    {
+        public string Type { get; set; }
+        public GeoObj Geometry { get; set; }
+        public PropObj Properties { get; set; }
+    }
+
+    public class GeoObj
+    {
+        public string Type { get; set; }
+        public int[] Coordinates { get; set; }
+
+    }
+
+    public class PropObj
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+    }
+}
---
+++ LINQInManhattan/LINQInManhattan/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using LINQInManhattan.Classes;
+
+namespace LINQInManhattan
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //      {
+            //      "type": "Feature",
+            //      "geometry": {
+            //          "type": "Point",
+            //          "coordinates": [
+            //              -73.999747,
+            //              40.742662
+            //          ]
+            //      },
+            //      "properties": {
+            //          "zip": "10011",
+            //          "city": "New York",
+            //          "state": "NY",
+            //          "address": "",
+            //          "borough": "Manhattan",
+            //          "neighborhood": "Chelsea",
+            //          "county": "New York County"
+            //      }
+            //      },
+
+            JObject dataObj = JObject.Parse(File.ReadAllText(@"../../../data.json"));
+            // JArray dataArray = (JArray)dataObj["Features"];
+            Console.WriteLine(dataObj);
+            // string dataString = dataObj.features;
+
+
+            // List<Entry> neighborhoods = JsonConvert.DeserializeObject<Entry>(dataObj["features"]);
+
+            //var queryData = from neighborhood in neighborhoods
+            //                select (string)neighborhood[];
+        }
+    }
+}
